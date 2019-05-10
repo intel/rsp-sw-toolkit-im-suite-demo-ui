de2f7bacbdff805974717f909b53c0fd472cfab0
adam-w-mitchell <adam.w.mitchell@intel.com>
2019-05-10 15:54:38 -0700

    websockets removed websocket io stuff

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,35 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable, Observer } from 'rxjs';
-import * as socketIo from 'socket.io-client';
-import { Socket } from '../shared/interfaces';
-
-declare var io: {
-  connect(url: string): Socket;
-};
+import { WebSocketSubject } from 'rxjs/webSocket';
 
 @Injectable()
 export class WebsocketService {
 
-  constructor() { }
+  private socket$: WebSocketSubject<any>;
 
-  socket: Socket;
-  observer: Observer<any>;
-
-  getQuotes(): Observable<any> {
-    this.socket = socketIo('http://localhost:8080');
-
-    this.socket.on('data', (res) => {
-      this.observer.next(res.data);
-    });
-
-    return this.createObservable();
+  constructor() {
+    this.socket$ = new WebSocketSubject('ws://localhost:8082');
   }
 
-  createObservable(): Observable<any> {
-    return new Observable<any>(observer => {
-
-      this.observer = observer;
-    });
+  getDataSub(): Observable<any> {
+    this.socket$
+    return this.socket$
   }
-
 }
--- src/app/theft-detection/theft-detection.component.ts
+++ src/app/theft-detection/theft-detection.component.ts
@@ -36,24 +36,34 @@ export class TheftDetectionComponent implements OnInit {
 
   sub: Subscription;
 
-  constructor(private chatService: BufferService, private websocketService: WebsocketService) { }
-
-  ngOnInit() {
-    this.sub = this.websocketService.getQuotes()
-      .subscribe(incomingData => {
-
-        let obj: data = JSON.parse(incomingData);
-        this.scaleTotal = obj.SCALE_item.total;
-        this.scaleUnit = obj.SCALE_item.units
-
-        this.posReadings = obj.POS_items;
-        this.scaleReading = obj.SCALE_item
-
-      });
+  constructor(private chatService: BufferService, private websocketService: WebsocketService) {
     this.posReadings = []
     this.scaleReading = {} as scaleItem
   }
 
+  ngOnInit() {
+    this.sub = this.websocketService.getDataSub()
+      .subscribe(
+
+        (message) => {
+          console.log(message)
+          message = JSON.stringify(message);
+
+          let obj: data = JSON.parse(message) as data;
+
+          this.scaleTotal = obj.SCALE_item.total;
+          this.scaleUnit = obj.SCALE_item.units
+
+          this.posReadings = obj.POS_items;
+          this.scaleReading = obj.SCALE_item;
+
+        },
+        (err) => console.error(err),
+        () => console.warn('Completed!')
+
+        // console.log(incomingData)
+      );
+  }
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
